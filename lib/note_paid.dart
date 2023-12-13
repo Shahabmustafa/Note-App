d693c1a5371aed6feaae34cd58fc6825b1c77f65
Shahabmustafa <shahabmustafa57@gmail.com>
2023-12-13 21:24:30 +0500

    get data to firebase

--- lib/note_paid.dart
+++ lib/note_paid.dart
@@ -16,78 +16,92 @@ class _NotePaidState extends State<NotePaid> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MasonryGridView.builder(
-          itemCount: 6,
-          mainAxisSpacing: 5,
-          crossAxisSpacing: 4,
-          gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          itemBuilder: (context,index){
-            return Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  height: 200,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.yellow,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            IconButton(
-                              onPressed: (){},
-                              icon: Icon(Icons.favorite_border),
-                            ),
-                            IconButton(
-                              onPressed: (){},
-                              icon: Icon(Icons.delete,color: Colors.red,),
-                            ),
-                          ],
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("notes").snapshots(),
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+            return MasonryGridView.builder(
+                itemCount: snapshot.data!.docs.length,
+                mainAxisSpacing: 5,
+                crossAxisSpacing: 4,
+                gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                itemBuilder: (context,index){
+                  return Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        height: 200,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.yellow,
                         ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 5),
                           child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Text(
-                                    "Heading",
-                                    style: GoogleFonts.aldrich(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 20,
-                                    ),
+                                  IconButton(
+                                    onPressed: (){},
+                                    icon: Icon(Icons.favorite_border),
+                                  ),
+                                  IconButton(
+                                    onPressed: (){},
+                                    icon: Icon(Icons.delete,color: Colors.red,),
                                   ),
                                 ],
                               ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                "Headingsdcnjfvnfjvnjvndfvvvjkfdvfnjkbkjgbkgbkgbvjnvjfnjnvjnjvnfjvnfvjdfvjvjvfn",
-                                style: GoogleFonts.aldrich(
-                                  fontWeight: FontWeight.w200,
-                                  fontSize: 15,
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 10),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          snapshot.data!.docs[index]["title"],
+                                          style: GoogleFonts.aldrich(
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 20,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          snapshot.data!.docs[index]["notes"],
+                                          style: GoogleFonts.aldrich(
+                                            fontWeight: FontWeight.w200,
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
                                 ),
                               ),
                             ],
                           ),
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-              ),
+                  );
+                }
             );
+          }else{
+            return Center(child: CircularProgressIndicator());
           }
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
