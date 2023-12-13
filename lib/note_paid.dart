f7cc21b43b5c1235fcf2d6d1f01e33941374a364
Shahabmustafa <shahabmustafa57@gmail.com>
2023-12-13 21:56:09 +0500

    create note pad

--- lib/note_paid.dart
+++ lib/note_paid.dart
@@ -13,9 +13,42 @@ class NotePaid extends StatefulWidget {
 class _NotePaidState extends State<NotePaid> {
   TextEditingController title = TextEditingController();
   TextEditingController notes = TextEditingController();
+
+  List<Color> textColor = [
+    Colors.white,
+    Colors.black,
+    Colors.white,
+    Colors.white,
+    Colors.white,
+    Colors.white,
+    Colors.white,
+    Colors.black,
+  ];
+  List<Color> colorScheme = [
+    Colors.red,
+    Colors.yellow,
+    Colors.grey,
+    Colors.green,
+    Colors.blue,
+    Colors.amber,
+    Colors.cyanAccent,
+    Colors.white,
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Notes"),
+        centerTitle: true,
+        // actions: [
+        //   IconButton(
+        //     onPressed: (){
+        //
+        //     },
+        //     icon: Icon(Icons.favorite_border),
+        //   ),
+        // ],
+      ),
       body: StreamBuilder(
         stream: FirebaseFirestore.instance.collection("notes").snapshots(),
         builder: (context,snapshot){
@@ -29,69 +62,86 @@ class _NotePaidState extends State<NotePaid> {
                 ),
                 itemBuilder: (context,index){
                   return Padding(
-                    padding: const EdgeInsets.all(2.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Container(
-                        height: 200,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          color: Colors.yellow,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: colorScheme[index],
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                              child: Column(
                                 children: [
-                                  IconButton(
-                                    onPressed: (){},
-                                    icon: Icon(Icons.favorite_border),
-                                  ),
-                                  IconButton(
-                                    onPressed: (){},
-                                    icon: Icon(Icons.delete,color: Colors.red,),
-                                  ),
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 10),
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      children: [
-                                        Text(
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Flexible(
+                                        child: Text(
                                           snapshot.data!.docs[index]["title"],
                                           style: GoogleFonts.aldrich(
                                             fontWeight: FontWeight.w600,
                                             fontSize: 20,
+                                            color: textColor[index],
                                           ),
                                         ),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      children: [
-                                        Text(
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Flexible(
+                                        child: Text(
                                           snapshot.data!.docs[index]["notes"],
                                           style: GoogleFonts.aldrich(
                                             fontWeight: FontWeight.w200,
                                             fontSize: 15,
+                                            color: textColor[index],
                                           ),
                                         ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                // IconButton(
+                                //   onPressed: (){
+                                //     final favourite = FirebaseFirestore.instance
+                                //         .collection("favourite")
+                                //         .doc(snapshot.data!.docs[index].id).set({
+                                //       "title" : snapshot.data!.docs[index]["title"],
+                                //       "notes" : snapshot.data!.docs[index]["notes"]
+                                //     });
+                                //
+                                //   },
+                                //   icon: Icon(Icons.favorite_border,color: textColor[index],),
+                                // ),
+                                IconButton(
+                                  onPressed: (){
+                                    FirebaseFirestore.instance
+                                        .collection("notes")
+                                        .doc(snapshot.data!.docs[index].id)
+                                        .delete();
+                                  },
+                                  icon: Icon(Icons.delete,color: textColor[index],),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
                     ),
@@ -139,10 +189,16 @@ class _NotePaidState extends State<NotePaid> {
                           color: Colors.white,
                         ),
                       ),
-                      onPressed: (){
-                        FirebaseFirestore.instance.collection("notes").add({
+                      onPressed: ()async{
+                        await FirebaseFirestore.instance.collection("notes").add({
                           "title" : title.text.trim(),
                           "notes" : notes.text.trim(),
+                        }).then((value){
+                          Navigator.pop(context);
+                          title.clear();
+                          notes.clear();
+                        }).onError((error, stackTrace){
+                          print(error);
                         });
                       },
                       child: Text("ADD NOTE"),
