042d4efb19d4fc9a687e916309e35d76be606189
Shahabmustafa <shahabmustafa57@gmail.com>
2023-12-13 21:10:08 +0500

    add note app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_calculator/note_paid.dart';
+import 'firebase_options.dart';
 
-import 'calculator.dart';
-
-void main() {
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  ColorScheme lightThemeColors(context) {
+    return const ColorScheme(
+      brightness: Brightness.light,
+      primary: Color(0xFF202020),
+      onPrimary: Color(0xFF505050),
+      secondary: Color(0xFFBBBBBB),
+      onSecondary: Color(0xFFEAEAEA),
+      error: Color(0xFFF32424),
+      onError: Color(0xFFF32424),
+      background: Color(0xFFF1F2F3),
+      onBackground: Color(0xFFFFFFFF),
+      surface: Color(0xFF54B435),
+      onSurface: Color(0xFF54B435),
+    );
+  }
 
   // This widget is the root of your application.
   @override
@@ -16,10 +37,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,
         primarySwatch: Colors.blue,
       ),
-      home: Calculator(),
+      home: NotePaid(),
     );
   }
 }
---
+++ lib/note_paid.dart
@@ -0,0 +1,146 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class NotePaid extends StatefulWidget {
+  const NotePaid({Key? key}) : super(key: key);
+
+  @override
+  State<NotePaid> createState() => _NotePaidState();
+}
+
+class _NotePaidState extends State<NotePaid> {
+  TextEditingController title = TextEditingController();
+  TextEditingController notes = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: MasonryGridView.builder(
+          itemCount: 6,
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 4,
+          gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemBuilder: (context,index){
+            return Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              onPressed: (){},
+                              icon: Icon(Icons.favorite_border),
+                            ),
+                            IconButton(
+                              onPressed: (){},
+                              icon: Icon(Icons.delete,color: Colors.red,),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Heading",
+                                    style: GoogleFonts.aldrich(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "Headingsdcnjfvnfjvnjvndfvvvjkfdvfnjkbkjgbkgbkgbvjnvjfnjnvjnjvnfjvnfvjdfvjvjvfn",
+                                style: GoogleFonts.aldrich(
+                                  fontWeight: FontWeight.w200,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          showModalBottomSheet(
+            context: context,
+            builder: (context){
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextFormField(
+                      controller: title,
+                      decoration: InputDecoration(
+                        hintText: "Title",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        )
+                      ),
+                    ),
+                    TextFormField(
+                      maxLines: 10,
+                      controller: notes,
+                      decoration: InputDecoration(
+                        hintText: "Notes",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        )
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: (){
+                        FirebaseFirestore.instance.collection("notes").add({
+                          "title" : title.text.trim(),
+                          "notes" : notes.text.trim(),
+                        });
+                      },
+                      child: Text("ADD NOTE"),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
